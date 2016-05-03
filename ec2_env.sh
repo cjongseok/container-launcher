ac34c8755ba33e826823c71213e1796c30875bdf
Jongseok Choi <cjongseok@gmail.com>
2016-05-03 08:27:49 +0000

    Update ec2_env.sh for setting up hostname

--- ec2_env.sh
+++ ec2_env.sh
@@ -24,6 +24,7 @@ function func_run_script_from_remote(){
 
 # Set EC2' Host Name
 #sudo hostname $(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+sudo hostname $(curl -s http://169.254.169.254/latest/meta-data/hostname)
 
 # install git
 echo "ec2=$AWS_INSTANCE"
